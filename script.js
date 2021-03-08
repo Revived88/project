862e928b057fda56b617401477825df9c964655d
Revived <tolikamv@gmail.com>
2021-03-08 12:36:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,127 @@
+"use sctrict";
+
+/*
+"use sctrict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+console.log(name);
+var name = 'Ivan';
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
+
+"use sctrict";
+let number = 4.6;
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = false;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+console.log(obj ["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6,'apple.bmp', {}, [] ];
+console.log(arr[1]);
+
+
+
+"use sctrict";
+//alert ('Hello');
+//const result = confirm( "are you here?");
+//console.log(result);
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+//console.log(answer + 5);
+
+const answers = [];
+
+//answers [0] = prompt('Как ваше имя?','');
+//answers [1] = prompt('Как ваша фамилия?','');
+//answers [2] = prompt('Сколько вам лет?','');
+
+//document.write(answers);
+
+console.log(typeof(answers));
+
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`)
+const user = "Ivan";
+alert(`Привет, ${user}!`);
+*/
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+console.log(4 + +" 5");
+
+
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+
+console.log(2+2*2=== '8');
+
+const isCheced = true,
+      isClose = true;
+//console.log(isCheced && isClose);
+console.log(isCheced || isClose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
